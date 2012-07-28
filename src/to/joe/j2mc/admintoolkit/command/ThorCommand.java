d64be39336483a4e7e134a875b610fa62ce7f8b3
Kieraan <kieraanbreeze@gmail.com>
2012-07-28 19:24:23 +0100

    Added /thor command

---
+++ src/to/joe/j2mc/admintoolkit/command/ThorCommand.java
@@ -0,0 +1,33 @@
+package to.joe.j2mc.admintoolkit.command;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import to.joe.j2mc.admintoolkit.J2MC_AdminToolkit;
+import to.joe.j2mc.core.command.MasterCommand;
+
+public class ThorCommand extends MasterCommand {
+
+    J2MC_AdminToolkit plugin;
+
+    public ThorCommand(J2MC_AdminToolkit toolkit) {
+        super(toolkit);
+        this.plugin = toolkit;
+    }
+
+    @Override
+    public void exec(CommandSender sender, String commandName, String[] args, Player player, boolean isPlayer) {
+        if (isPlayer) {
+            String possibleGod = player.getName().toLowerCase();
+            if (this.plugin.thor.contains(possibleGod)) {
+                this.plugin.thor.remove(possibleGod);
+                player.sendMessage(ChatColor.GOLD + "You lose your mystical powers");
+            } else {
+                this.plugin.thor.add(possibleGod);
+                player.sendMessage(ChatColor.GOLD + "You gain mystical powers");
+            }
+        }
+    }
+
+}
