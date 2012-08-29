41c8130292dfa1e75b5fcf56f05be7e893ed5ced
mbax <matt@phozop.net>
2012-08-29 11:21:49 -0400

    Remove command bloat

--- src/to/joe/j2mc/admintoolkit/J2MC_AdminToolkit.java
+++ src/to/joe/j2mc/admintoolkit/J2MC_AdminToolkit.java
@@ -32,7 +32,6 @@ public class J2MC_AdminToolkit extends JavaPlugin implements Listener {
         this.getCommand("time").setExecutor(new TimeCommand(this));
         this.getCommand("mob").setExecutor(new MobCommand(this));
         this.getCommand("coo").setExecutor(new CooCommand(this));
-        this.getCommand("getflags").setExecutor(new GetFlagsCommand(this));
         this.getCommand("storm").setExecutor(new StormCommand(this));
         this.getCommand("whois").setExecutor(new WhoIsCommand(this));
         this.getCommand("gm").setExecutor(new GameModeToggleCommand(this));
