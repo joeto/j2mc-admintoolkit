3d14d716d4e9677d24e6583148d030e44376c801
jamietech <jamie@jamiete.ch>
2013-01-09 17:23:05 +1000

    ItemMeta is confusing. Thanks, Eviltechie.

--- src/to/joe/j2mc/admintoolkit/command/KickaxeCommand.java
+++ src/to/joe/j2mc/admintoolkit/command/KickaxeCommand.java
@@ -7,6 +7,7 @@ import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import to.joe.j2mc.admintoolkit.J2MC_AdminToolkit;
 import to.joe.j2mc.core.command.MasterCommand;
@@ -26,16 +27,20 @@ public class KickaxeCommand extends MasterCommand<J2MC_AdminToolkit> {
                 player.sendMessage(ChatColor.GOLD + "Your gold axe seems less cool now.");
                 for (ItemStack i : player.getInventory().getContents()) {
                     if (i.getItemMeta().getDisplayName().equals(ChatColor.LIGHT_PURPLE + "KickAxe")) {
-                        i.getItemMeta().setDisplayName(null);
-                        i.getItemMeta().setLore(null);
+                        ItemMeta meta = i.getItemMeta();
+                        meta.setDisplayName(null);
+                        meta.setLore(null);
+                        i.setItemMeta(meta);
                     }
                 }
             } else {
                 this.plugin.kickAxe.add(username);
                 player.sendMessage(ChatColor.GOLD + "You rub your gold axe.");
                 ItemStack kickAxe = new ItemStack(Material.GOLD_AXE, 1);
-                kickAxe.getItemMeta().setDisplayName(ChatColor.LIGHT_PURPLE + "KickAxe");
-                kickAxe.getItemMeta().setLore(Arrays.asList(new String[] { "This special axe will kick players", "that are hit with it.", "Use it carefully." }));
+                ItemMeta meta = kickAxe.getItemMeta();
+                meta.setDisplayName(ChatColor.LIGHT_PURPLE + "KickAxe");
+                meta.setLore(Arrays.asList(new String[] { "This special axe will kick players", "that are hit with it.", "Use it carefully." }));
+                kickAxe.setItemMeta(meta);
                 player.setItemInHand(kickAxe);
             }
         }
